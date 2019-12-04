b9a4320bb5ff35518958485f9c4940d64a6b1828
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-04 12:29:27 +0300

    create django form and create the forms for the three models

---
+++ artist/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from . models import Profile, Comments, Post
+
+class PostImagesForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'date']
+
+
+class PostComments(forms.ModelForm):
+    class Meta: 
+        model = Comments
+        exclude = ['image','posted','user']
+        
+class PostProfile(forms.ModelForm):
+    class Meta:
+        models = Profile
+        exclude = ['user',]
+
+    
