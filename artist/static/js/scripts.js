004e82c01414d614044b8ab4d637e2efc282beec
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-04 10:37:32 +0300

    add class model profile and navbar

--- artist/static/js/scripts.js
+++ artist/static/js/scripts.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
-    alert('hello Brian')
-    $("h2").click(function() {
-        alert("This is a header.");
-    });
-
-    $("p").click(function() {
-        alert("This is a paragraph.");
+$(function() {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
     });
 });
