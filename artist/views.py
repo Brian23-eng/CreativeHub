53fd8bb64d3aa84927b8b4ecfea748547301e67c
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-04 12:49:20 +0300

    add django_registration module

--- artist/views.py
+++ artist/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     title = 'Creative || Hub'
     
     return render(request, 'index.html',{'title': title})
+
+@login_required(login_url='login')
+def photos(request):
+    posts = Post.all_posts()
+    return render(request, 'photo.html',{'posts':posts})
 
 
