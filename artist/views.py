9411e96b83e5ad22babf9042486f90684051e0d8
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-05 09:42:14 +0300

    add a function that edits profile and its corresponding templates

--- artist/views.py
+++ artist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from . models import Post, Profile, Comments
 from . forms import PostComments, PostImagesForm,PostProfile
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -29,13 +30,28 @@ def post_image(request):
         posts = Post.objects.all()
     except Post.DoesNotExist:
         posts = None
-    return rebder(request,'post_image.html',{'posts': posts, 'forms':forms})
+    return render(request,'post_image.html',{'posts': posts, 'forms':forms})
 
-@login_required
-def logoutView(request):
+def edit_profile(request, username):
+    user = User.objetcs.get(username=username)
     if request.method == 'POST':
-        logout(request)
-        print('loged out')
-    return render(request, 'home.html')
+        user_form = PostProfile(instance=request.user)
+        prof_form = PostProfile(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.user_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+        
+    else:
+        user_form = PostProfile(instance=request.user)
+        prof_form = PostProfile(instance=request.user.profile)
+        
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    
+    return render(request, 'edit.html', params)
+
 
 
